d2668a59202af6297069ca4c88c3a39fe0bf8dca
Anna Pettersen <aopettersen@gmail.com>
2019-02-19 15:46:43 +0000

    Created destroy method + refined controller code

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,12 @@
 class TasksController < ApplicationController
+
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @tasks = Task.find(params[:id])
   end
 
   def new
@@ -17,18 +19,25 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @tasks = Task.find(params[:id])
   end
 
   def update
-    @tasks = Task.find(params[:id])
     @tasks.update(task_params)
     redirect_to tasks_path(@tasks)
+  end
+
+  def destroy
+    @tasks.destroy
+    redirect_to tasks_path
   end
 
   private
 
   def task_params
-    params.require(:task).permit(:title, :details)
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @tasks = Task.find(params[:id])
   end
 end
