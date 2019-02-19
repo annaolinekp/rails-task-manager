ad594c5ad6921af5f1955a66530b23a2b7d7ef09
Anna Pettersen <aopettersen@gmail.com>
2019-02-19 14:44:27 +0000

    Added check_box to tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,19 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @tasks = Task.find(params[:id])
+  end
+
+  def new
+    @tasks = Task.new
+  end
+
+  def create
+    @tasks = Task.find(params[:id])
+    Task.create(task_params)
+    redirect_to task_path
+  end
 end
